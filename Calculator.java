f2fe0f3c1183d8b296336ecafb0dd7fe5a58b7ef
mariop410 <mpanagrosso410@gmail.com>
2026-02-05 19:26:58 -0500

    ask user for two whole numbers and print out the sum

--- Calculator.java
+++ Calculator.java
@@ -1,10 +1,19 @@
-import java.util.Scanner
+import java.util.Scanner;
 
 public class Calculator {
 
 public static void main(String[] args) {
     Scanner scanner = new Scanner(System.in);
 
+    System.out.println("Enter a whole number:");
+    int num1 = scanner.nextInt();
+
+    System.out.println("Enter another whole number:");
+    int num2 = scanner.nextInt();
+
+    int result = num1 + num2;
+
+    System.out.println("The sum is " + result);
 }
 
 }
