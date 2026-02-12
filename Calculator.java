ad0474f8c2d56ce4a536e70954d57586ba1c0af6
mariop410 <mpanagrosso410@gmail.com>
2026-02-12 15:22:35 -0500

    added error message if user enters 0 for the second number and chooses division

--- Calculator.java
+++ Calculator.java
@@ -5,10 +5,10 @@ public class Calculator {
 public static void main(String[] args) {
     Scanner scanner = new Scanner(System.in);
 
-    System.out.println("Enter a whole number:");
+    System.out.println("Enter a number:");
     double num1 = scanner.nextDouble();
 
-    System.out.println("Enter another whole number:");
+    System.out.println("Enter another number:");
     double num2 = scanner.nextDouble();
 
     System.out.println("Woukd you like to add. subtract, multiply, or divide the two numbers?");
@@ -30,8 +30,14 @@ public static void main(String[] args) {
     }
 
     else if (answer.equals("divide")) {
-        double result = num1 / num2;
-        System.out.println("The quotient is " + result);
+        if (num2 == 0) {
+            System.out.println("Error: Divide by Zero");
+        } 
+        else {
+            double result = num1 / num2;
+            System.out.println("The quotient is " + result);
+        }
+        
     }
 }
 
